3ce623b56e30e02d4c374c417ef5b7aafabe987a
simon kamau <https://github.com/SIMONKAMAU21/own-git-.git>
2024-01-30 21:52:44 -0800

    third commit

--- src/Components/Complete.jsx
+++ src/Components/Complete.jsx
@@ -1,40 +1,20 @@
 // import { useState } from "react";
-// import "./Complete.scss";
+import "./Complete.scss";
 
-// const Complete = () => {
-//   const Range = (0) => {
-//     const[rangeValue,setRangeValue]=useState(80);
-//     const handleRangeChange = (event)=>{
-//       setRangeValue(event.target.value);
-//     }
-//   }
-//   return (
-//     <>
-//     <div>
-//       <input
-//       type= 'range'
-//       id='rangeInput'
-//       min='0'
-//       max='100'
-//       value={rangeValue}
-//       onChange={handleRangeChange}
-//       />
-//       <p>{handleRangeChange} </p>
-//       <p>{rangeValue} </p>
-//     </div>
+const Complete = () => {
+  return (
+   <>
+      <div className="compelete-profile">
+        <div className="content">
+          <h6>complete your profile</h6>
+          <div className="first">
+            <hr />
+            <p>80%</p>
+          </div>
+        </div>
+      </div> 
+    </>
+  );
+    };
 
-    
-//       {/* <div className="compelete-profile">
-//         <div className="content">
-//           <h6>complete your profile</h6>
-//           <div className="first">
-//             <hr />
-//             <p>80%</p>
-//           </div>
-//         </div>
-//       </div> */}
-//     </>
-//   );
-//     };
-
-// export default Complete;
+export default Complete;
